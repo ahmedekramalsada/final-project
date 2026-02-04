b851ece50be6ab68715080e592c0a771a514f386
Ahmed ekram <Ahmedekramalsada@gmail.com>
2026-02-04 20:53:35 +0200

    edit

--- final-project-nti/app/server.js
+++ final-project-nti/app/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const port = process.env.PORT || 3000;
 
-// The URI is retrieved from the environment (Step 18) 
-const mongoURI = process.env.MONGODB_URI;
+app.get('/', (req, res) => {
+  res.send('Hello World! This is a simple app build and push demo.');
+});
 
-mongoose.connect(mongoURI)
-  .then(() => console.log("Connected to MongoDB Atlas"))
-  .catch(err => console.error("Connection error", err));
-
-app.get('/', (req, res) => res.send('DevOps Intern Project: CI/CD Active!'));
-app.listen(3000, () => console.log('App listening on port 3000'));
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
