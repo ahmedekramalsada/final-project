1d5b8075e625f7153f7d7e52229a68aa27d7f559
Ahmed ekram <Ahmedekramalsada@gmail.com>
2026-02-10 19:03:30 +0200

    commnet the secret connection

--- final-project-nti/terraform/infrastructure/secrets.tf
+++ final-project-nti/terraform/infrastructure/secrets.tf
@@ -1,8 +1,8 @@
 # Fetch secrets from Vault
-data "vault_kv_secret_v2" "db_creds" {
-  mount = "kv"            # Adjust mount path if necessary (e.g., "secret")
-  name  = "production/db" # Adjust secret path
-}
+# data "vault_kv_secret_v2" "db_creds" {
+#   mount = "kv"            # Adjust mount path if necessary (e.g., "secret")
+#   name  = "production/db" # Adjust secret path
+# }
 
 # Example output for debugging (Removed in production)
 # output "db_username_debug" {
